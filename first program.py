51d63aa667ce2891eea00703e4acb19ad4b31f84
Sanket Desai <sanketvrdesai@gmail.com>
2020-06-03 15:11:56 -0400

    modified

--- first program.py
+++ first program.py
@@ -7,7 +7,13 @@ age = int(input("Please enter your age: "))
 
 print("Your name:"+"\t"+str(age))
 
+number=int(input("Enter the number to repat the statements: "))
+
 if age<100:
-    year_born = 2020 - age
-    year_turn_100 = year_born+100
-    print("The year your turn 100:\t"+str(year_turn_100))
+    if 0<number<100:
+        year_born = 2020 - age
+        year_turn_100 = year_born+100
+        while number!=0:
+            print("The year your turn 100:\t"+str(year_turn_100))
+            number=number-1
+            
